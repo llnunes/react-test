47d5b59c9db8e67c13e0dcbb0e9f759eca713bc7
lucas.lopes <lucas.nunes@ana.gov.br>
2018-10-18 13:14:03 -0300

    Initial commit

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nos' ></PageHeader>
+        <h2>TESTE</h2>
+        <p>TEste teste</p>
+        
+    </div>
+)
