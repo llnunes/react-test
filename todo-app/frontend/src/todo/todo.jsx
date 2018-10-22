457ac506802fea97759e8c915861e977e03f2706
lucas.lopes <lucas.nunes@ana.gov.br>
2018-10-22 18:07:24 -0300

    Adicionado novas funcionalidades.

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,23 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import PageHeader from '../template/pageHeader'
 import TodoForm from '../todo/todoForm'
 import TodoList from '../todo/todoList'
 
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component {
     constructor(props) {
         super(props)
+        this.state = {description: '', list: []}
+        this.handleChange = this.handleChange.bind(this)         
         this.handleAdd = this.handleAdd.bind(this)
     }
 
     handleAdd() {
-        console.log(this)
+        const description = this.state.description
+        axios.post(URL, {description})
+        .then(resp => console.log('Funcionou!'))
     }
-
+    handleChange (e) {
+        this.setState({...this.state, description: e.target.value})
+    }
     render () {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' ></PageHeader>
-                <TodoForm handleAdd={this.handleAdd} />
+                <TodoForm 
+                    description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
