5d1e6e139eccd4f608e113acde8107f9a87fb375
lucas.lopes <lucas.nunes@ana.gov.br>
2018-10-23 18:58:12 -0300

    Atualizado.

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -12,15 +12,40 @@ export default class Todo extends Component {
         this.state = {description: '', list: []}
         this.handleChange = this.handleChange.bind(this)         
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+
+        this.refresh()
+    }
+
+    refresh () {
+        axios.get(`${URL}?sort=-createdAt`)
+        .then(resp => this.setState({...this.state, description: '', list: resp.data}))
     }
 
     handleAdd() {
         const description = this.state.description
         axios.post(URL, {description})
-        .then(resp => console.log('Funcionou!'))
+        .then(resp => this.refresh())
     }
     handleChange (e) {
         this.setState({...this.state, description: e.target.value})
+    }
+
+    handleRemove (todo) {
+        axios.delete (`${URL}/${todo._id}`)
+        .then(resp => this.refresh())
+    }
+
+    handleMarkAsDone (todo) {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: true})
+        .then(resp => this.refresh())
+    }
+
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: false})
+        .then(resp => this.refresh())
     }
     render () {
         return (
@@ -30,7 +55,10 @@ export default class Todo extends Component {
                     description={this.state.description}
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd} />
-                <TodoList />
+                <TodoList list={this.state.list} 
+                    handleRemove={this.handleRemove}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending} />
             </div>
         )
     }
